520117e712c03e7837d8c9cfcdb0937b2209fdca
Ian Murfin <ianmurfin@protonmail.com>
2023-01-08 15:40:06 +0000

    setup index buffer for a square

--- BasicSetup/Source/main.cc
+++ BasicSetup/Source/main.cc
@@ -265,7 +265,8 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	};
 
 	ID3D11InputLayout* inputLayout{};
-	ID3D11Buffer* triangleVertBuffer{};
+	ID3D11Buffer* vertexBuffer{};
+	ID3D11Buffer* indexBuffer{};
 	ID3D11VertexShader* vertexShader{};
 	ID3D11PixelShader* pixelShader{};
 
@@ -308,31 +309,54 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 			Terminate();
 		}
 
-		Vertex triangle[] = {
-			Vertex{{0.0f, 0.5f, 0.5f}, {1.0f, 0.0f, 0.0f, 1.0f}},
+		Vertex squareVertices[]{
+			Vertex{{-0.5f, -0.5f, 0.5f}, {1.0f, 0.0f, 0.0f, 1.0f}},
+			Vertex{{-0.5f, 0.5f, 0.5f}, {0.0f, 1.0f, 0.0f, 1.0f}},
+			Vertex{{0.5f, 0.5f, 0.5f}, {0.0f, 0.0f, 1.0f, 1.0f}},
 			Vertex{{0.5f, -0.5f, 0.5f}, {0.0f, 1.0f, 0.0f, 1.0f}},
-			Vertex{{-0.5f, -0.5f, 0.5f}, {0.0f, 0.0f, 1.0f, 1.0f}},
+		};
+
+		DWORD squareIndices[] = {
+			0, 1, 2,
+			0, 2, 3,
 		};
 
 		D3D11_BUFFER_DESC vertexBufferDesc;
 		ZeroMemory(&vertexBufferDesc, sizeof(vertexBufferDesc));
 		vertexBufferDesc.Usage = D3D11_USAGE_DEFAULT;
-		vertexBufferDesc.ByteWidth = sizeof(Vertex) * 3;
+		vertexBufferDesc.ByteWidth = sizeof(Vertex) * 4;
 		vertexBufferDesc.BindFlags = D3D11_BIND_VERTEX_BUFFER;
 		vertexBufferDesc.CPUAccessFlags = 0;
 		vertexBufferDesc.MiscFlags = 0;
 
 		D3D11_SUBRESOURCE_DATA vertexBufferData;
 		ZeroMemory(&vertexBufferData, sizeof(vertexBufferData));
-		vertexBufferData.pSysMem = triangle;
-		if(d3dDevice->CreateBuffer(&vertexBufferDesc, &vertexBufferData, &triangleVertBuffer) != S_OK) {
-			LogMsg("Failed creating vertex buffer for triangle");
+		vertexBufferData.pSysMem = squareVertices;
+		if(d3dDevice->CreateBuffer(&vertexBufferDesc, &vertexBufferData, &vertexBuffer) != S_OK) {
+			LogMsg("Failed creating vertex buffer");
 			Terminate();
 		}
 
-		UINT stride = sizeof(Vertex);
-		UINT offset = 0;
-		d3dDeviceCtx->IASetVertexBuffers(0, 1, &triangleVertBuffer, &stride, &offset);
+		D3D11_BUFFER_DESC indexBufferDesc;
+		ZeroMemory(&indexBufferDesc, sizeof(indexBufferDesc));
+		indexBufferDesc.Usage = D3D11_USAGE_DEFAULT;
+		indexBufferDesc.ByteWidth = sizeof(DWORD) * 2 * 3;
+		indexBufferDesc.BindFlags = D3D11_BIND_INDEX_BUFFER;
+		indexBufferDesc.CPUAccessFlags = 0;
+		indexBufferDesc.MiscFlags = 0;
+
+		D3D11_SUBRESOURCE_DATA indexBufferData;
+		ZeroMemory(&indexBufferData, sizeof(indexBufferData));
+		indexBufferData.pSysMem = squareIndices;
+
+		if(d3dDevice->CreateBuffer(&indexBufferDesc, &indexBufferData, &indexBuffer) != S_OK) {
+			LogMsg("Failed creating index buffer");
+			Terminate();
+		}
+
+		UINT stride = sizeof(Vertex), offset = 0;
+		d3dDeviceCtx->IASetIndexBuffer(indexBuffer, DXGI_FORMAT_R32_UINT, 0);
+		d3dDeviceCtx->IASetVertexBuffers(0, 1, &vertexBuffer, &stride, &offset);
 
 		auto result = d3dDevice->CreateInputLayout(layout, numElements, vsBlob->GetBufferPointer(),
 			vsBlob->GetBufferSize(), &inputLayout);
@@ -375,12 +399,13 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 					rgbDir[i] *= -1;
 			}
 			d3dDeviceCtx->ClearRenderTargetView(renderTargetView, rgb);
-			d3dDeviceCtx->Draw(3, 0);
+			d3dDeviceCtx->DrawIndexed(6, 0, 0);
 			d3dSwapChain->Present(0, 0);
 		}
 	}
 
-	triangleVertBuffer->Release();
+	vertexBuffer->Release();
+	indexBuffer->Release();
 	vertexShader->Release();
 	pixelShader->Release();
 	vertexShader->Release();
